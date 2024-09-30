0808df289b880224617c1989b5879317f3f940ee
Pablo Bustos <pablobustos609@gmail.com>
2024-09-30 19:36:27 -0300

    add daisyui and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
 
   return (
-    <>
+    <div className="p-4 h-screen flex items-center justify-center">
       <h1 className="text-red-500 text-6xl">Hola</h1>
-    </>
+      <button className="btn btn-primary">Press</button>
+    </div>
   )
 }
 
