860e514e056e9c9d461016cc7862815565270ea6
Pablo Bustos <pablobustos609@gmail.com>
2024-10-01 00:07:31 -0300

    create sidebar section and add react icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
+import Sidebar from "../../components/Sidebar"
+
 const index = () => {
   return (
-    <div>home</div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      {/* <MessageContainer /> */}
+    </div>
   )
 }
 
